1cb79fb4c27433d3e0353fa64370f7bd47f7c87a
Mufafa98 <marian.parlea2@gmail.com>
2024-04-09 21:23:25 +0300

    User Existing Check
    Added Connection setup
    Added query for checking if an user exists at login

---
+++ src/controllers/Login/AuthGeneral.js
@@ -0,0 +1,34 @@
+// TO DO: Manage exceptions acordingly
+
+const mysql = require("mysql");
+
+function establishConnection() {
+	// Create a connection to the MySQL server
+	const connection = mysql.createConnection({
+		host: "152.53.32.81",
+		user: "dev",
+		password: "pXkZkGCsxdaHsr6",
+		database: "development",
+	});
+	return connection;
+}
+function canUserLogin(email, password, connection) {
+	connection.connect((err) => {
+		if (err) throw err;
+		connection.query(
+			`SELECT verifiedEmail FROM User WHERE password LIKE '${password}' AND emailPrimary LIKE '${email}'`,
+			(err, result) => {
+				if (err) throw err;
+				let verified = 0;
+				if (result[0].verifiedEmail === 1) {
+					console.log("User verified");
+					verified = 1;
+				} else {
+					console.log("User unverifeid");
+				}
+				connection.end();
+				return verified;
+			},
+		);
+	});
+}
