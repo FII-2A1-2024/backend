d7aaa52afa7fc6afef0e3bb776ea332a10fa387b
Cristian Bozomitu <cristianbozomitu202@gmail.com>
2024-04-11 20:08:39 +0300

    Added endpoint for testing purposes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const postRoutes = require("./routes/postRoutes");
 const signUpRouter = require('./routes/signUp')
+const jwtSecretHandler = require('./utils/JWT/JWTSecretGeneration')
+const authenticateToken = require('./utils/JWT/JWTAuthentication')
+const generateAccessToken = require('./utils/JWT/JWTGeneration')
+const jwt = require('jsonwebtoken')
 
 
 app
@@ -11,4 +15,3 @@ app
 
 
 module.exports = app;
-
