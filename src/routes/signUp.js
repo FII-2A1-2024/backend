f18936938452b4e77fbaea83f7493155e3d161eb
rzv16 <razvanboita1609@gmail.com>
2024-04-18 12:39:13 +0300

    Am extras logica din router intr-un controller pt trimitere email

--- src/routes/signUp.js
+++ src/routes/signUp.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const signUpController = require('../controllers/signUpController')
-const jwt = require('jsonwebtoken')
-const existsInDB = require('../models/existsInDb')
-const userIsVerified = require('../models/isVerified')
-const markUserAsVerified = require('../models/markVerified')
+const emailController = require('../controllers/emailController')
 const HttpCodes = require('../config/returnCodes')
-const signUpService = require('../services/signUpService')
+
 
 router
 .get("/exists", (req, res) => {
@@ -14,46 +11,8 @@ router
 })
 
 .get('/verify', async (req, res) => {
-    const token = req.query.token;
-    // console.log("S-a intrat in verify")
-    try {
-        const decoded = jwt.verify(token, signUpService.privateKey);
-        const currTime = Date.now()
-        const tokenStamp = decoded.timestamp;
-        const maxVerifyTime = 24 * 60 * 60 * 1000; //24 hr in milisecunde
-        if(currTime - tokenStamp > maxVerifyTime){
-            res.status(HttpCodes.TOKEN_EXPIRED).send('Verification token expired')
-            return
-        }
-        
-        //RIGHT HERE
-        const email = decoded.email
-        
-        //!exista userul??
-        const userExistsResult = await existsInDB(email);
-        if (!userExistsResult) {
-            res.status(HttpCodes.DOESNOT_EXIST).send('User does not exist');
-            console.log("Eroare de user does not exist");
-            return;
-        }
-
-        //! e deja verificat userul??
-        const userIsVerifiedResult = await userIsVerified(email);
-        if (userIsVerifiedResult) {
-            res.status(HttpCodes.ALREADY_VERIFIED).send('You are already verified');
-            console.log("Eroare de user already verified");
-            return;
-        }
-        //TODO verifica userul
-        await markUserAsVerified(email);
-        console.log("Totul e ok si userul a fost verificat");
-
-        //? Cand e totul ok
-        res.send("Your account has been succesfully verified")
-    } catch(error){
-        console.error("Error verifying token: ", error);
-        res.status(HttpCodes.INVALID_REQUEST).send("<p>Invalid token</p><a href='#'>Resend</a>")
-    }
+    emailController(req, res)
+    // res.status(status).send(toSend)
 })    
 .post("/", (req, res) => {
     console.log("req: ", req.body)
