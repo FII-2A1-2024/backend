fc1745e5fc21e47b0bc21834a3fdc79beb4f04f9
Teodora Chiriac <teodorachiriac73@gmail.com>
2024-04-23 22:29:46 +0300

    modified app

--- src/app.js
+++ src/app.js
@@ -6,26 +6,17 @@ const signUpRouter = require('./routes/signUp')
 const jwtSecretHandler = require('./utils/JWT/JWTSecretGeneration')
 const authenticateToken = require('./utils/JWT/JWTAuthentication')
 const generateAccessToken = require('./utils/JWT/JWTGeneration')
-<<<<<<< HEAD
-const jwt = require('jsonwebtoken');
-const adminRoutes = require("./routes/adminRoutes");
-=======
 const loginRouter = require("./routes/login");
 const jwt = require('jsonwebtoken')
->>>>>>> fe97d095fb2329399564d0c78601660fbb4a9c2d
+const adminRoutes = require("./routes/adminRoutes");
 
 
 app
     .use(express.json())
     .use("/signup", signUpRouter)
     .use(postRoutes)
-<<<<<<< HEAD
-    .use(adminRoutes)
-
-=======
     .use("/login", loginRouter)
     .use(commentRoutes);
-    
->>>>>>> fe97d095fb2329399564d0c78601660fbb4a9c2d
+    .use(adminRoutes)
 
 module.exports = app;
