0d8c1a2043741d66dd892572f44854b147d2b082
Teodora Chiriac <teodorachiriac73@gmail.com>
2024-04-26 17:20:09 +0300

    .

--- src/models/existsInDb.js
+++ src/models/existsInDb.js
@@ -1,32 +1,24 @@
-const { PrismaClient } = require("@prisma/client");
-const HttpCodes = require("../config/returnCodes.js");
-const bcrypt = require('bcrypt');
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt')
+
 const prisma = new PrismaClient();
 
-// Verifica daca un email exista in baza de date, caz in care
-// verifica daca se potriveste cu parola. Codul generat este
-// returnat
-async function existsInDBEmailAndPass(email, password) {
-	try {
-		const user = await prisma.user.findUnique({
-			where: {
-				emailPrimary: email,
-			},
-		});
-		const arePasswordsTheSame = await bcrypt.compare(password, user.password);
-		
-		if (user == null) {
-			return HttpCodes.USER_DOESNOT_EXIST;
-		}
-		if (!arePasswordsTheSame) {
-			return HttpCodes.WRONG_PASSWORD;
-		}
-		return HttpCodes.SUCCES;
-	} catch (error) {
-		console.error("Error retrieving user:", error);
-	} finally {
-		await prisma.$disconnect();
-	}
-}
+async function existsInDB(username) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                emailPrimary: username
+            }
+        });
+        return user!==null;
 
-module.exports = existsInDBEmailAndPass;
+    } catch (error) {
+        console.error('Error retrieving user:', error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+//? Exemplu de usage
+// existsInDB("newUser");
+
+module.exports = existsInDB
