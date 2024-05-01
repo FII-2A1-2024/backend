d9c0959de25c9d444817a5dc7bde5f8d85289983
Cristian Bozomitu <cristianbozomitu202@gmail.com>
2024-05-02 01:36:57 +0300

    [BE2-031] Simplified checkExistence function logic

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -2,22 +2,19 @@ const HttpCodes = require("../config/returnCodes.js");
 const UserService = require('../services/userServices.js')
 
 async function checkExistence(email, password) {
-	let code = HttpCodes.SUCCESS;
-	//TODO
 	const userExistsInDB = await UserService.validCredentials(email, password);
-	if (userExistsInDB === HttpCodes.SUCCESS) {
-		const userVerified = await UserService.isVerifed(email);
-		if (userVerified) {
-			code = HttpCodes.SUCCESS;
-		} else {
-			code = HttpCodes.UNVERIFIED_EMAIL;
-		}
-	} else if (userExistsInDB === HttpCodes.USER_DOESNOT_EXIST) {
-		code = userExistsInDB;
-	} else if (userExistsInDB === HttpCodes.WRONG_PASSWORD) {
-		code = userExistsInDB;
+
+	if (userExistsInDB !== HttpCodes.SUCCESS) {
+		return userExistsInDB;
 	}
-	return code;
+
+	const userVerified = await UserService.isVerifed(email);
+
+	if (!userVerified) {
+		return HttpCodes.UNVERIFIED_EMAIL;
+	}
+
+	return HttpCodes.SUCCESS;
 }
 
 module.exports = checkExistence;
