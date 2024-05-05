bf43e47b72e15cc72e78ce62a88e5bee85492254
Teodora Chiriac <teodorachiriac73@gmail.com>
2024-05-05 03:24:38 +0300

    [BE2-010] adding adminRoutes to app.js

--- src/app.js
+++ src/app.js
@@ -1,30 +1,13 @@
+
 const express = require("express");
 const app = express();
 const resetPasswordRoutes = require("./routes/resetPassword");
 const postRoutes = require("./routes/postRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 const postFollowRoutes = require("./routes/postFollowRoutes");
-<<<<<<< HEAD
-const signUpRouter = require('./routes/signUp')
-const jwtSecretHandler = require('./utils/JWT/JWTSecretGeneration')
-const authenticateToken = require('./utils/JWT/JWTAuthentication')
-const generateAccessToken = require('./utils/JWT/JWTGeneration')
-const loginRouter = require("./routes/login");
-const jwt = require('jsonwebtoken')
+
 const adminRoutes = require("./routes/Admin&UserRoutes/adminRoutes");
 const isUser = require("./utils/Middleware/isUser");
-
-app
-    .use(express.json())
-    .use("/signup", signUpRouter)
-    .use("/login", loginRouter)
-    .use("/resetPass", resetPassword)
-    //we need token here, also the path remains the same, however i put the prefix like this 
-    //for the isUser to function correctly 
-    .use("/posts",authenticateToken,isUser,postRoutes) 
-    .use("/comments",authenticateToken,isUser,commentRoutes)
-    .use(adminRoutes) 
-=======
 
 const cors = require('cors')
 const signUpRoutes = require('./routes/signUp')
@@ -41,7 +24,6 @@ app
     .use(resetPasswordRoutes)
     .use(commentRoutes)
     .use(optionsRoutes)
-    .use(postFollowRoutes);
-    
->>>>>>> cd13b46b7b0f7067c424317fdeb6ed68e8683143
+    .use(postFollowRoutes)
+    .use(adminRoutes);
 module.exports = app;
