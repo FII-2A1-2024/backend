e901c2d11fcdab75efbf25e75f86340f0c1d97df
Cristian Bozomitu <cristianbozomitu202@gmail.com>
2024-05-11 16:18:06 +0300

    [BE2-021] Preliminary implementation of account deletion feature

--- src/controllers/Login/loginController.js
+++ src/controllers/Login/loginController.js
@@ -20,6 +20,7 @@ async function login(req, res) {
 	try {
 		const { email, password, socket } = req.body;
 		let code = HttpCodes.SUCCESS;
+
 		if (email === undefined || password === undefined || socket === undefined) {
 			handleErrorCodes(res, HttpCodes.BAD_REQUEST);
 			return;
--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -9,16 +9,16 @@ async function signUp(req, res){
         let username = req.body.email;
 
         //Vom normaliza emailul in cazul in care pune si litere mari
-        username = normalizeEmail(username)
+       // username = normalizeEmail(username)
         // console.log("Normalized username: ", username);
         let code = HttpCodes.SUCCESS
-        if(!verifyEmailSyntax(username)){
+        //if(!verifyEmailSyntax(username)){
             //emailul e invalid, nu are sintaxa buna
-            code = HttpCodes.INVALID_EMAIL
-            console.log("Nu introducem userul in baza de date, are email gresit");
-        } else{
+           // code = HttpCodes.INVALID_EMAIL
+           // console.log("Nu introducem userul in baza de date, are email gresit");
+       // } else{
             code = await signUpService.createUser(username, password)
-        }
+        //}
         res.send({resCode: code})
     } catch (error){
         res.status(500).send({error : error.message})
