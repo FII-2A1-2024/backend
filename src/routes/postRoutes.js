15f2994e9b185429f8efa99dea8676de2ac4cc79
ffilip <ffilip@bitdefender.com>
2024-05-11 15:56:40 +0300

    [BE1-028]: added support for deleting file from post

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -7,5 +7,6 @@ router.get("/posts/all", postController.getAll);
 router.post("/posts", postController.post);
 router.put("/posts", postController.put);
 router.delete("/posts", postController.delete);
+router.delete("/posts/file", postController.deleteFile);
 
 module.exports = router;
