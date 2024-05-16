54ff5382aafa91d0320d22c481706c213e265e3b
Mufafa98 <marian.parlea2@gmail.com>
2024-05-16 21:40:28 +0300

    [BE2-032] : CORS allow all origins

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const resetPasswordRoutes = require("./routes/resetPassword");
@@ -9,21 +8,21 @@ const postFollowRoutes = require("./routes/postFollowRoutes");
 const adminRoutes = require("./routes/Admin&UserRoutes/adminRoutes");
 const isUser = require("./utils/Middleware/isUser");
 
-const cors = require('cors')
-const signUpRoutes = require('./routes/signUp')
+const cors = require("cors");
+const signUpRoutes = require("./routes/signUp");
 const loginRoutes = require("./routes/login");
-const optionsRoutes = require('./routes/options')
+const optionsRoutes = require("./routes/options");
 
-app.use(cors())
+app.use(cors({ origin: "*" }));
 
 app
-    .use(express.json())
-    .use(signUpRoutes)
-    .use(postRoutes)
-    .use(loginRoutes)
-    .use(resetPasswordRoutes)
-    .use(commentRoutes)
-    .use(optionsRoutes)
-    .use(postFollowRoutes)
-    .use(adminRoutes);
+	.use(express.json())
+	.use(signUpRoutes)
+	.use(postRoutes)
+	.use(loginRoutes)
+	.use(resetPasswordRoutes)
+	.use(commentRoutes)
+	.use(optionsRoutes)
+	.use(postFollowRoutes)
+	.use(adminRoutes);
 module.exports = app;
