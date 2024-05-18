2086f5a65f88a55f5a8d3a6e9128a73d09aa08d1
ffilip <ffilip@bitdefender.com>
2024-05-18 22:38:56 +0300

    [BE1-016]: added direct messages functionality

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 const path = require("node:path");
 const dotenv = require("dotenv");
 const app = require("./app");
-
+const Socket = require("./utils/SocketIO");
 const http = require("node:http");
-const { Server } = require("socket.io");
 
 const envPath = path.resolve(__dirname, "config", ".env.local");
 dotenv.config({ path: envPath });
@@ -14,22 +13,8 @@ const port = Number.parseInt(process.env.SERVER_PORT);
 // Create HTTP server
 const server = http.createServer(app);
 
-// Create Socket.io instance and attach it to the server with CORS settings
-const io = new Server(server, {
-	cors: {
-		origin: "*",
-		methods: ["GET", "POST"],
-	},
-});
-
-io.on("connection", (socket) => {
-	console.log("A user connected");
-
-	// Handle socket events
-	socket.on("disconnect", () => {
-		console.log("User disconnected");
-	});
-});
+// Initialize Socket.io
+const io = Socket.initializeSocket(server);
 
 server.listen(port, ip, () => {
 	console.log(`Server is listening at http://${ip}:${port}`);
