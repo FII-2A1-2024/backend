df27ce7ae1aefdf7008234703d0fcd0b0172748f
Mufafa98 <46997399+Mufafa98@users.noreply.github.com>
2024-05-26 16:57:52 +0300

    Update verifyEmailSyntax.js

--- src/utils/verifyEmailSyntax.js
+++ src/utils/verifyEmailSyntax.js
@@ -1,6 +1,5 @@
 function verifyEmailSyntax(email) {
-	const emailRegex =
-		/^(?:[a-zA-Z]+\.[a-zA-Z]+@student\.uaic\.ro|[a-zA-Z]+@info\.uaic\.ro)|[a-zA-Z]+@profs\.uaic\.ro)$/;
+	const emailRegex = /^[a-zA-Z]+(?:.[a-zA-Z]+)?@(student|profs).uaic.ro$/;
 	return emailRegex.test(email);
 	//trebuie sa fie de forma: {orice}.{orice}@student.uaic.ro
 	// sau {orice}@info.uaic.ro
