e0fc1aabe657bd685b9da607160e9b80bdef79a2
Teodora Chiriac <teodorachiriac73@gmail.com>
2024-06-02 01:00:29 +0300

    [BE2-035] added socket routes for delete&add

---
+++ src/controllers/readdSocketController.js
@@ -0,0 +1,59 @@
+const userServices = require('../services/userServices')
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+async function deleteLoggedUser(req, res) {
+    try {
+        const socket_id = req.body.socket
+        const socketExists = await prisma.loggedUsers.findMany({
+            where: {
+                socket: socket_id
+            }
+        });
+        //console.log(socketExists)
+        if (socketExists.length === 0) {
+            return res.json({ message: "wrong socket id" });
+        }
+
+        const result = await userServices.deleteUserFromLoggedTable(socket_id);
+        return res.json(result);
+    } catch (error) {
+        res.status(500).send({ error: error.message })
+    }
+
+
+}
+
+async function addLoggedUser(req, res) {
+    try {
+        const userExists = await prisma.user.findUnique({
+            where: {
+                uid: req.body.uid
+            }
+        });
+        if (!userExists) {
+            return res.json({ message: "wrong user id" });
+        }
+        const user = {
+            uid: req.body.uid,
+            username: req.body.username,
+            socket: req.body.socket,
+        };
+        const userExistsInLoggedTable = await prisma.loggedUsers.findMany({
+            where: {
+                uid: req.body.uid,
+                username: req.body.username,
+                socket: req.body.socket,
+            }
+        });
+        if (!(userExistsInLoggedTable.length === 0)) {
+            return res.json({ message: "user already exists in loggedUsers" });
+        }
+        const result = await userServices.logUserIn(user);
+        return res.json(result);
+    } catch (error) {
+        res.status(500).send({ error: error.message })
+    }
+}
+
+
+module.exports = { deleteLoggedUser, addLoggedUser }
