a0e4690f10e06c767084fa21dabbdaad202d4945
Teodora Chiriac <teodorachiriac73@gmail.com>
2024-06-04 14:41:07 +0300

    [BE2-016]delete unnecessary prisma model and file

--- src/routes/Admin&UserRoutes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-const authenticateToken = require("../../utils/JWT/JWTAuthentication");
-const isUser= require("../../utils/Middleware/isUser"); 
-const express = require("express");
-const router = express.Router();
-//to be implemented
-module.exports= router;
